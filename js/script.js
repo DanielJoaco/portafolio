6e229abcf3f9e1e5a94987b5f9b4943da20f21da
DanielJoaco <danieljorjuela@gmail.com>
2024-05-02 11:30:47 -0500

    se creo el repositorio del portafolio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    loadScripts()
+})
+
+class Skills {
+    constructor(img, alt, text, type, category, url = ''){
+        this.img = img
+        this.alt = alt
+        this.text = text
+        this.type = type
+        this.category = category
+        this.url = url
+    }
+}
+
+
+function loadScripts(){
+
+}
