f861f69d8b132293f9e1850d421280dc71720b73
DanielJoaco <danieljorjuela@gmail.com>
2024-05-05 20:36:55 -0500

    Se creo el entorno de despliegue para github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import DataLoader from './components/DataLoader';
 import Navbar from './components/Navbar';
 import StartSection from './components/StartSection';
